74c3ca7830b980eb6528338863037b1d3c46fbd6
OscarGB <oscar.gomez.borz@gmail.com>
2017-09-22 12:38:06 +0200

    main con control de errores y cambio de argumentos en el .h

--- Tercero/Redes 1/P1/practica1.h
+++ Tercero/Redes 1/P1/practica1.h
@@ -7,16 +7,17 @@
 
 /*Codigos de salida del programa*/
 #define EXIT_OK = 0
-#define EXIT_FAIL = -1
+#define EXIT_ERROR = -1
 
 /*Codigos de salida de la funcion de lectura de paquetes pcap*/
-#define PCAP_SUCCES = 1
-#define PCAP_TIMEOUT = 0
+#define PCAP_OK = 1
 #define PCAP_ERROR = -1
+#define PCAP_TIMEOUT = 0
 #define PCAP_FILE_END = -2
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #ifndef PRACTICA1_H
 #define PRACTICA1_H
@@ -39,11 +40,11 @@ Nombre:
 Descripcion:
     Captura en vivo los paquetes de la interfaz eth0
 Entrada:
-    char* num: número de bytes a mostrar por paquete.
+    int num: número de bytes a mostrar por paquete, tiene que ser positivo.
 Salida:
     int, será el código de error que devolverá el programa completo
 ************************************************************/
-int live_capture(char* num);
+int live_capture(int num);
 
 /***************************************************************
 Nombre:
@@ -51,11 +52,11 @@ Nombre:
 Descripcion:
     Analiza la traza de un archivo .pcap
 Entrada:
-    char* num: número de bytes a mostrar por paquete.
-    char* trace: traza pcap a analizar.
+    int num: número de bytes a mostrar por paquete, tiene que ser positivo.
+    char* trace: traza pcap a analizar, tiene que ser un archivo .pcap.
 Salida:
     int, será el código de error que devolverá el programa completo
 ************************************************************/
-int pcap_analyze(char* num, char* trace);
+int pcap_analyze(int, char* trace);
 
 #endif
