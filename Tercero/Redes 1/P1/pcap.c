c217b4061719dcc8db429748ccd0974cbf905c3c
nagomez97 <nagomez97@gmail.com>
2017-09-26 16:13:27 -0500

    hola

--- Tercero/Redes 1/P1/pcap.c
+++ Tercero/Redes 1/P1/pcap.c
@@ -117,7 +117,7 @@ int live_capture(int num){
 
 	sprintf(file_name, "eth0.%lu.pcap", (unsigned long)time(NULL));
 
-	if ((desc = pcap_open_live("wlan0", ETH_FRAME_MAX, PROMISCUO, TIMEOUT, errbuf)) == NULL){
+	if ((desc = pcap_open_live("eth0", ETH_FRAME_MAX, PROMISCUO, TIMEOUT, errbuf)) == NULL){
 		fprintf(stdout, "Error: No se pudo abrir la interfaz eth0.\n");
 		return EXIT_ERROR;
 	}
