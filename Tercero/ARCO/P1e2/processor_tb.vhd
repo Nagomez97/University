4e10ca9d487fb9b4af115fc0d528d2762bcdc343
nagomez97 <nagomez97@gmail.com>
2017-10-05 13:06:25 +0200

    hola

--- Tercero/ARCO/P1e2/processor_tb.vhd
+++ Tercero/ARCO/P1e2/processor_tb.vhd
@@ -13,8 +13,8 @@ use ieee.std_logic_unsigned.all;
 
 entity processor_tb is
    generic (
-      INIT_FILENAME_INST : string   := "C:\Users\Nacho\Desktop\University\Tercero\ARCO\p1\instrucciones"; -- Fichero con las instrucciones
-      INIT_FILENAME_DATA : string   := "C:\Users\Nacho\Desktop\University\Tercero\ARCO\p1\datos";         -- Fichero con los datos
+      INIT_FILENAME_INST : string   := "instrucciones"; -- Fichero con las instrucciones
+      INIT_FILENAME_DATA : string   := "datos";         -- Fichero con los datos
       N_CYCLES           : positive := 100              -- Numero de ciclos a ejecutar
    );
 end processor_tb;
