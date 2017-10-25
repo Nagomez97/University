227336cabad87c00bb240d7f1956dad2498bf6a4
nagomez97 <nagomez97@gmail.com>
2017-10-25 11:10:00 +0200

    p2 no se si funciona

--- Tercero/ARCO/P2/P2e1/processor.vhd
+++ Tercero/ARCO/P2/P2e1/processor.vhd
@@ -129,6 +129,7 @@ architecture rtl of processor is
 	-- Registro 1 segmentacion
 	signal reg1_PC4_out : std_logic_vector(31 downto 0);
 	signal reg1_IDataIn_out: std_logic_vector(31 downto 0);
+	signal reg1_reset : std_logic; -- Se usa para borrar la instruccion almacenada en el reg1 cuando el branch es efectivo
 
 	-- Registro 2 segmentacion
 	signal reg2_IDataIn_out : std_logic_vector(31 downto 0);
@@ -399,9 +400,9 @@ begin
    ------------------------------------------------------
    -- Registro 1 segmentacion
    ------------------------------------------------------
-   	process(PC4, Clk, Reset, IDataIn, reg1_Stop)
+   	process(PC4, Clk, Reset, IDataIn, reg1_Stop, reg1_reset)
    	begin
-   			if Reset = '1' then
+   			if Reset = '1'  or reg1_reset = '0' then
    					reg1_IDataIn_out <= (others => '0');
    					reg1_PC4_out <= (others => '0');
    			elsif rising_edge(Clk)  and reg1_Stop /= '1' then
@@ -583,14 +584,17 @@ begin
    -- En este caso hay que parar el pipeline un ciclo.
    ------------------------------------------------------
 
-	process(reg2_MemRead_out, reg2_RegDst_out, reg1_IDataIn_out)
+	process(reg2_MemRead_out, reg2_RegDst_out, reg1_IDataIn_out, Branch, ZFlag)
 	begin
+
+		-- Condiciones de parada ante un lw seguido de una instruccion que requiera informacion del load
+
 		if reg2_MemRead_out = '1' then --Comprobación de que es un lw
-			if (reg2_RegDst_out = reg1_IDataIn_out(25 downto 21)) AND (reg2_RegDst_out /= "00000") then --Comprobación registro rs=rd
+			if (reg2_RegDst_out == reg1_IDataIn_out(25 downto 21)) AND (reg2_RegDst_out /= "00000") then --Comprobación registro rs=rd
 				PC_Stop <= '1';
 				reg1_Stop <= '1';
 				bubble <= '1';
-			elsif (reg2_RegDst_out = reg1_IDataIn_out(20 downto 16)) AND (reg2_RegDst_out /= "00000") then
+			elsif (reg2_RegDst_out == reg1_IDataIn_out(20 downto 16)) AND (reg2_RegDst_out /= "00000") then
 				PC_Stop <= '1';
 				reg1_Stop <= '1';
 				bubble <= '1';
@@ -603,6 +607,40 @@ begin
 			PC_Stop <= '0';
 			reg1_Stop <= '0';
 			bubble <= '0';
+		end if;
+
+		-- Condiciones de parada cuando tenemos un branch
+
+		if Branch = '1' then -- Comprobamos, antes de llegar al segundo registro, si la instruccion va a ser un salto
+							 -- Esto lo hacemos para que, si es un salto, no se ejecute la siguiente instruccion
+			PC_Stop <= '1'; -- Queremos parar el PC para que, en caso de no producirse el salto, no se salte instrucciones
+			reg1_Stop <= '1'; -- La instruccion siguiente al branch se queda en el registro 1 a la espera de conocer la resolucion del salto
+			bubble <= '0';
+
+			if reg2_Branch_out = '1' then -- Esto es para que, si hay dos branch consecutivos, cuando el primero pase a ejecucion se meta un bubble
+										  -- en el registro 2
+				bubble <= '1';
+			else
+				bubble <= '0';
+			end if;
+		else if reg2_Branch_out = '1' then -- Cuando el branch pase a ejecucion queremos que se cree un bubble y se siga manteniendo
+										   -- el bloqueo del PC y del Registro 1
+			PC_Stop <= '1';
+			reg1_Stop <= '1';
+			bubble <= '1';
+
+			if ZFlag = '1' then -- Si el salto es efectivo (los registros son iguales y, por tanto, ZFlag vale '1') queremos borrar
+								-- la instruccion almacenada en el registro 1
+				reg1_reset <= '1';
+			else
+				reg1_ reset <= '0';
+
+		else
+			PC_Stop <= '0';
+			reg1_Stop <= '0';
+			bubble <= '0';
+			reg1_reset <= '0';
+
 		end if;
 	end process;
 
