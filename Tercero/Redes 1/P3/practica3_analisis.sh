e139dc47f82487ed020e97c64a3a62cce6c43c4a
OscarGB <oscar.gomez.borz@gmail.com>
2017-11-03 12:45:48 +0100

    avances

--- Tercero/Redes 1/P3/practica3_analisis.sh
+++ Tercero/Redes 1/P3/practica3_analisis.sh
@@ -6,6 +6,9 @@
 
 # El primer argumento debe ser el nombre de la traza a analizar
 NOMBRE_TRAZA=$1
+TEMPORAL="practica3.temp"
+TOP=10
+RESULTADOS="practica3.res"
 
 # Comprobamos que se haya introducido un argumento
 if [ "$#" -ne 1 ] ; then 
@@ -31,6 +34,7 @@ echo "Porcentajes de paquetes IP y no IP"
 
 # Contamos los paquetes que son IP 
 #PAQ_IP=`tshark -r $NOMBRE_TRAZA -T fields -e  eth.type -e vlan.etype -Y 'eth.type eq 0x00000800 or vlan.etype eq 0x00000800' | wc -l`
+PAQ_IP=`tshark -r $NOMBRE_TRAZA -T fields -e ip.dst -e ip.src -e udp.dstport -e udp.srcport -e tcp.dstport -e tcp.srcport -e frame.len -Y 'eth.type eq 0x00000800 or vlan.etype eq 0x00000800' > $TEMPORAL | wc -l`
 PAQ_IP=55114
 PAQ_TOTAL=55667
 # Contamos los paquetes totales
@@ -75,7 +79,51 @@ echo "--------------------------------------------------"
 echo ""
 
 ##########################################################################
-# Calculamos el top 10 en bytes de: 
+# Calculamos el top 10 en bytes y en paquetes de: 
 # - Direcciones IP (origen/destino)
-# - Puertos (origen/destino)
+# - Puertos (origen/destino) separando TCP de UDP
 ##########################################################################
+
+# TOP 10 en paquetes:
+# Para ello hemos guardado en $TEMPORAL un filtrado de Tshark con los campos pedidos y
+# a continuacion los ordenamos en funcion de la columna que nos interese. Los agrupamos
+# (usando uniq) y contamos los grupos. La variable TOP indica el numero de lineas a mostrar.
+# Tenemos en cuenta que puede haber lineas vacias. Las eliminamos con sed
+
+echo "TOPS 10 por paquetes" > $RESULTADOS
+
+# ip.dst
+echo "Top 10 IP destino" >> $RESULTADOS
+awk -F '\t' '{print $1}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk '{printf "%10s%40s\n",$1, $2}' | head -n $TOP >> $RESULTADOS
+# ip.src
+echo "Top 10 IP origen" >> $RESULTADOS
+awk -F '\t' '{print $2}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk '{printf "%10s%40s\n",$1, $2}' | head -n $TOP >> $RESULTADOS
+# udp.dstport
+echo "Top 10 Puerto UDP destino" >> $RESULTADOS
+awk -F '\t' '{print $3}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk '{printf "%10s%40s\n",$1, $2}' | head -n $TOP >> $RESULTADOS
+# udp.srcport
+echo "Top 10 Puerto UDP origen" >> $RESULTADOS
+awk -F '\t' '{print $4}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr |  awk '{printf "%10s%40s\n",$1, $2}' |head -n $TOP >> $RESULTADOS
+# tcp.dstport
+echo "Top 10 Puerto TCP destino" >> $RESULTADOS
+awk -F '\t' '{print $5}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr |  awk '{printf "%10s%40s\n",$1, $2}' |head -n $TOP >> $RESULTADOS
+# tcp.srcport
+echo "Top 10 Puerto TCP origen" >> $RESULTADOS
+awk -F '\t' '{print $6}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr |  awk '{printf "%10s%40s\n",$1, $2}' |head -n $TOP >> $RESULTADOS
+
+# TOP 10 en bytes
+# Para ello utilizamos el mismo fichero temporal que antes. Seleccionamos las columnas deseadas
+# y las agrupamos por el parametro. Utilizamos awk para sumar los tamaños y ordenamos por el tamaño.
+# Mostramos el TOP con head
+
+echo "TOPS 10 por bytes" >> $RESULTADOS
+
+# ip.dst
+echo "Top 10 IP destino" >> $RESULTADOS
+awk -F '\t' '{print $1 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | head -n $TOP >> $RESULTADOS
+
+#Mostramos los resultados
+cat $RESULTADOS
+
+#eliminamos el temporal
+#rm $TEMPORAL
