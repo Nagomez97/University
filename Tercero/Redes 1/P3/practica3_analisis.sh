e26648c4fa5d8e393fad1650617e540946600c66
OscarGB <oscar.gomez.borz@gmail.com>
2017-11-04 21:21:54 +0100

    Arreglos en el formato y feeding parcial

--- Tercero/Redes 1/P3/practica3_analisis.sh
+++ Tercero/Redes 1/P3/practica3_analisis.sh
@@ -7,6 +7,7 @@
 # El primer argumento debe ser el nombre de la traza a analizar
 NOMBRE_TRAZA=$1
 TEMPORAL="practica3.temp"
+TOTAL="total.temp"
 TOP=10
 RESULTADOS="practica3.res"
 
@@ -27,56 +28,61 @@ fi
 ##########################################################################
 # Obtenemos los porcentajes de los paquetes de tipo IP, ya sean VLAN o no.
 ##########################################################################
-echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Porcentajes de paquetes IP y no IP"
+echo "" > $RESULTADOS
+echo "--------------------------------------------------" >> $RESULTADOS
+echo "" >> $RESULTADOS
+echo "Porcentajes de paquetes IP y no IP" >> $RESULTADOS
+
+# Contamos los paquetes totales
+echo "Analizando todos los paquetes"
+PAQ_TOTAL=`tshark -r $NOMBRE_TRAZA -T fields -e frame.len > $TOTAL | wc -l`
 
 # Contamos los paquetes que son IP 
-#PAQ_IP=`tshark -r $NOMBRE_TRAZA -T fields -e  eth.type -e vlan.etype -Y 'eth.type eq 0x00000800 or vlan.etype eq 0x00000800' | wc -l`
+echo "Analizando paquetes IP"
 PAQ_IP=`tshark -r $NOMBRE_TRAZA -T fields -e ip.dst -e ip.src -e udp.dstport -e udp.srcport -e tcp.dstport -e tcp.srcport -e frame.len -Y 'eth.type eq 0x00000800 or vlan.etype eq 0x00000800' > $TEMPORAL | wc -l`
 PAQ_IP=55114
 PAQ_TOTAL=55667
-# Contamos los paquetes totales
-#PAQ_TOTAL=`tshark -r $NOMBRE_TRAZA | wc -l`
+
 
 # Obtenemos los paquetes no IP
 PAQ_NOIP=$(($PAQ_TOTAL - $PAQ_IP))
 
-# Obtenemos e imprimimos los porcentajes de los paquetes
-awk "BEGIN {printf \"IP: %.2f%%\n\", (($PAQ_IP*100)/$PAQ_TOTAL);}"
-awk "BEGIN {printf \"NOIP: %.2f%%\n\", (($PAQ_NOIP*100)/$PAQ_TOTAL);}"
+# Obtenemos e imprimimos en los resultados los porcentajes de los paquetes
+awk "BEGIN {printf \"IP: %.2f%%\n\", (($PAQ_IP*100)/$PAQ_TOTAL);}" >> $RESULTADOS
+awk "BEGIN {printf \"NO IP: %.2f%%\n\", (($PAQ_NOIP*100)/$PAQ_TOTAL);}" >> $RESULTADOS
 
-echo ""
-echo ""
+echo "" >> $RESULTADOS
+echo "" >> $RESULTADOS
 
 ##########################################################################
 # Obtenemos los porcentajes de los paquetes con protocolo TCP 
 # y UDP respecto al total de paquetes IP
 ##########################################################################
 
-echo "Porcentajes de paquetes UDP, TCP y OTROS respecto al total de paquetes IP"
+echo "Porcentajes de paquetes UDP, TCP y OTROS respecto al total de paquetes IP" >> $RESULTADOS
 
 PAQ_TCP=49378
 PAQ_UDP=4970
 
 # Contamos los paquetes con protocolo TCP
+echo "Analizando paquetes TCP"
 #PAQ_TCP=`tshark -r $NOMBRE_TRAZA -T fields -e ip.proto -Y 'ip.proto eq 6' | wc -l`
 
 # Contamos los paquetes con protocolo UDP
+echo "Analizando paquetes UDP"
 #PAQ_UDP=`tshark -r $NOMBRE_TRAZA -T fields -e ip.proto -Y 'ip.proto eq 17' | wc -l`
 
 # Obtenemos el numero de paquetes no TCP ni UDP
 PAQ_OTROS=$(($PAQ_IP - $PAQ_TCP - $PAQ_UDP))
 
-# Obtenemos e imprimimos los porcentajes de los paquetes
-awk "BEGIN {printf \"IP-TCP: %.2f%%\n\", (($PAQ_TCP*100)/$PAQ_IP);}"
-awk "BEGIN {printf \"IP-UDP: %.2f%%\n\", (($PAQ_UDP*100)/$PAQ_IP);}"
-awk "BEGIN {printf \"IP-OTROS: %.2f%%\n\", (($PAQ_OTROS*100)/$PAQ_IP);}"
+# Obtenemos e imprimimos en los resultados los porcentajes de los paquetes
+awk "BEGIN {printf \"IP-TCP: %.2f%%\n\", (($PAQ_TCP*100)/$PAQ_IP);}" >> $RESULTADOS
+awk "BEGIN {printf \"IP-UDP: %.2f%%\n\", (($PAQ_UDP*100)/$PAQ_IP);}" >> $RESULTADOS
+awk "BEGIN {printf \"IP-OTROS: %.2f%%\n\", (($PAQ_OTROS*100)/$PAQ_IP);}" >> $RESULTADOS
 
-echo ""
-echo "--------------------------------------------------"
-echo ""
+echo "" >> $RESULTADOS
+echo "--------------------------------------------------" >> $RESULTADOS
+echo "" >> $RESULTADOS
 
 ##########################################################################
 # Calculamos el top 10 en bytes y en paquetes de: 
@@ -90,26 +96,41 @@ echo ""
 # (usando uniq) y contamos los grupos. La variable TOP indica el numero de lineas a mostrar.
 # Tenemos en cuenta que puede haber lineas vacias. Las eliminamos con sed
 
-echo "TOPS 10 por paquetes" > $RESULTADOS
+echo "TOPS 10 por paquetes" >> $RESULTADOS
+echo "" >> $RESULTADOS
 
 # ip.dst
-echo "Top 10 IP destino" >> $RESULTADOS
-awk -F '\t' '{print $1}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk '{printf "%10s%40s\n",$1, $2}' | head -n $TOP >> $RESULTADOS
+echo "Top 10 IPs destino" >> $RESULTADOS
+echo "  pos  paquetes                              ip destino" >> $RESULTADOS
+awk -F '\t' '{print $1}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' | head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
 # ip.src
-echo "Top 10 IP origen" >> $RESULTADOS
-awk -F '\t' '{print $2}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk '{printf "%10s%40s\n",$1, $2}' | head -n $TOP >> $RESULTADOS
+echo "Top 10 IPs origen" >> $RESULTADOS
+echo "  pos  paquetes                               ip origen" >> $RESULTADOS
+awk -F '\t' '{print $2}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' | head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
 # udp.dstport
-echo "Top 10 Puerto UDP destino" >> $RESULTADOS
-awk -F '\t' '{print $3}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk '{printf "%10s%40s\n",$1, $2}' | head -n $TOP >> $RESULTADOS
+echo "Top 10 Puertos UDP destino" >> $RESULTADOS
+echo "  pos  paquetes                      puerto udp destino" >> $RESULTADOS
+awk -F '\t' '{print $3}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' | head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
 # udp.srcport
-echo "Top 10 Puerto UDP origen" >> $RESULTADOS
-awk -F '\t' '{print $4}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr |  awk '{printf "%10s%40s\n",$1, $2}' |head -n $TOP >> $RESULTADOS
+echo "Top 10 Puertos UDP origen" >> $RESULTADOS
+echo "  pos  paquetes                       puerto udp origen" >> $RESULTADOS
+awk -F '\t' '{print $4}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
 # tcp.dstport
-echo "Top 10 Puerto TCP destino" >> $RESULTADOS
-awk -F '\t' '{print $5}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr |  awk '{printf "%10s%40s\n",$1, $2}' |head -n $TOP >> $RESULTADOS
+echo "Top 10 Puertos TCP destino" >> $RESULTADOS
+echo "  pos  paquetes                      puerto tcp destino" >> $RESULTADOS
+awk -F '\t' '{print $5}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
 # tcp.srcport
-echo "Top 10 Puerto TCP origen" >> $RESULTADOS
-awk -F '\t' '{print $6}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr |  awk '{printf "%10s%40s\n",$1, $2}' |head -n $TOP >> $RESULTADOS
+echo "Top 10 Puertos TCP origen" >> $RESULTADOS
+echo "  pos  paquetes                       puerto tcp origen" >> $RESULTADOS
+awk -F '\t' '{print $6}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+echo "--------------------------------------------------" >> $RESULTADOS
+echo "" >> $RESULTADOS
 
 # TOP 10 en bytes
 # Para ello utilizamos el mismo fichero temporal que antes. Seleccionamos las columnas deseadas
@@ -117,13 +138,42 @@ awk -F '\t' '{print $6}' $TEMPORAL | sed '/^\s*$/d' | sort | uniq -c | sort -nr 
 # Mostramos el TOP con head
 
 echo "TOPS 10 por bytes" >> $RESULTADOS
-
+echo "" >> $RESULTADOS
 # ip.dst
-echo "Top 10 IP destino" >> $RESULTADOS
-awk -F '\t' '{print $1 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | head -n $TOP >> $RESULTADOS
+echo "Top 10 IPs destino" >> $RESULTADOS
+echo "  pos    tamaño                              ip destino" >> $RESULTADOS
+awk -F '\t' '{print $1 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+# ip.src
+echo "Top 10 IPs origen" >> $RESULTADOS
+echo "  pos    tamaño                               ip origen" >> $RESULTADOS
+awk -F '\t' '{print $2 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+# udp.dst
+echo "Top 10 Puertos UDP destino" >> $RESULTADOS
+echo "  pos    tamaño                      puerto udp destino" >> $RESULTADOS
+awk -F '\t' '{print $3 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+# udp.src
+echo "Top 10 Puertos UDP origen" >> $RESULTADOS
+echo "  pos    tamaño                       puerto udp origen" >> $RESULTADOS
+awk -F '\t' '{print $4 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+# tcp.dst
+echo "Top 10 Puertos TCP destino" >> $RESULTADOS
+echo "  pos    tamaño                      puerto tcp destino" >> $RESULTADOS
+awk -F '\t' '{print $5 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+# tcp.src
+echo "Top 10 Puertos TCP origen" >> $RESULTADOS
+echo "  pos    tamaño                       puerto tcp origen" >> $RESULTADOS
+awk -F '\t' '{print $6 "\t" $7}' practica3.temp | sort | awk 'NR>1 && p!=$1 {printf "%10s%40s\n", s, p; s=0} {s+=$2} {p=$1}' | sort -nr | awk 'BEGIN{n=0}{printf "%5s%10s%40s\n", n+=1, $1, $2}' |head -n $TOP >> $RESULTADOS
+echo "" >> $RESULTADOS
+echo "--------------------------------------------------" >> $RESULTADOS
+echo "" >> $RESULTADOS
 
 #Mostramos los resultados
 cat $RESULTADOS
 
 #eliminamos el temporal
-#rm $TEMPORAL
+rm $TEMPORAL $TOTAL
