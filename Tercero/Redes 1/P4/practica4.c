6b609de33f291aba926e5ee46c4bb6fea31f74fb
nagomez97 <nagomez97@gmail.com>
2017-12-13 16:14:20 +0100

    hola

--- Tercero/Redes 1/P4/practica4.c
+++ Tercero/Redes 1/P4/practica4.c
@@ -419,7 +419,7 @@ uint8_t moduloIP(uint8_t* segmento, uint64_t longitud, uint16_t* pila_protocolos
 	/*Bucle para fragmentacion*/
 	for(offset=0; offset<longitud; offset+=max_tam){
 		/*No es el ultimo paquete*/
-		if(offset + max_tam < longitud){
+		if(offset + (max_tam-IHL*4) < longitud){
 			total_size = htons(max_tam + (IHL*4));
 			flags_pos = ((offset/8) & 0x1fff) | 0x2000;
 		}
